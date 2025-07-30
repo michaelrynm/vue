b733c90dfcf80d6a449527b0267732ce4ec1ddac
michaelynm <michaelmarcelino55@gmail.com>
2025-07-30 12:16:01 +0700

    feat(app): import About us page

--- landing-page/src/App.vue
+++ landing-page/src/App.vue
@@ -1,47 +1,19 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Hero from './components/Hero.vue'
+import About from './components/About.vue'
+import Projects from './components/Projects.vue'
+import Help from './components/Help.vue'
+import Team from './components/Team.vue'
+import Citizen from './components/Citizen.vue'
+import Partners from './components/Partners.vue'
+import Footer from './components/Footer.vue'
+import AboutUs from './pages/AboutUs.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <AboutUs />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
