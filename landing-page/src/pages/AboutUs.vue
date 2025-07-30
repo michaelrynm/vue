07416aa87e80e8bec0862a1e08b8ca7caa758d53
michaelynm <michaelmarcelino55@gmail.com>
2025-07-30 12:16:22 +0700

    feat(about us): import components for about us page

---
+++ landing-page/src/pages/AboutUs.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Hero from '@/components/Hero.vue'
+import About from '@/components/About.vue'
+import Projects from '@/components/Projects.vue'
+import Help from '@/components/Help.vue'
+import Team from '@/components/Team.vue'
+import Citizen from '@/components/Citizen.vue'
+import Partners from '@/components/Partners.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<template>
+  <main>
+    <Hero />
+    <About />
+    <Projects />
+    <Help />
+    <Team />
+    <Citizen />
+    <Partners />
+    <Footer />
+  </main>
+</template>
+
+<style scoped></style>
